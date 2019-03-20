94146df6b37754f147ad5d1dfb5b2afa1793464f
veadaodao <46975802+veadaodao@users.noreply.github.com>
2019-03-20 13:06:21 -0400

    q
    q

---
+++ exercises/midterm/Assets/Scripts/lose.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class lose : MonoBehaviour
+{
+    public Text loseText;
+
+    private int villagedamage;
+    void Start()
+    {
+        villagedamage = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        loseText.text = "";
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "dragon")
+        {
+
+            setloseText();
+            villagedamage = villagedamage + 1;
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.useGravity = false;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            other.gameObject.SetActive(false);
+            
+     
+    
+
+
+
+        }
+
+    }
+    private void setloseText()
+    {
+        loseText.text = "You lose";
+    }
+}
